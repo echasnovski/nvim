db402aa41852740b3cc0e736bd15c28bc519b3a0
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2021-07-06 21:00:47 +0300

    Update 'mini.completion'.

--- lua/mini/completion.lua
+++ lua/mini/completion.lua
@@ -47,15 +47,15 @@ function MiniCompletion.auto_complete()
   H.timers.auto_complete:stop()
 
   local char_is_lsp_trigger = H.is_lsp_trigger(vim.v.char)
-  if H.pumvisible() or
+  if vim.fn.pumvisible() > 0 or
     not (H.is_char_keyword(vim.v.char) or char_is_lsp_trigger) then
     MiniCompletion.stop_trigger()
     return
   end
 
-  -- If character is purely lsp trigger, make new LSP requests without fallback
-  if char_is_lsp_trigger then H.cancel_lsp_requests() end
-  H.cache.do_fallback = not char_is_lsp_trigger
+  -- If character is purely lsp trigger, make new LSP request without fallback
+  if char_is_lsp_trigger then H.cancel_lsp_request() end
+  H.do_fallback = not char_is_lsp_trigger
 
   -- Using delay (of debounce type) seems to actually improve user experience
   -- as it allows fast typing without many popups. Also useful when synchronous
@@ -66,27 +66,29 @@ function MiniCompletion.auto_complete()
 end
 
 function MiniCompletion.force_complete(fallback)
-  H.cache.do_fallback = fallback or true
+  MiniCompletion.stop_trigger()
+  H.do_fallback = fallback or true
   H.trigger()
 end
 
 function MiniCompletion.stop_trigger()
   H.timers.auto_complete:stop()
-  H.cancel_lsp_requests()
-  H.cache = {}
+  H.cancel_lsp_request()
+  H.do_fallback = true
 end
 
 function MiniCompletion.complete_lsp(findstart, base)
-  -- Don't do anyhting if no LSP client is attached
-  if not H.has_lsp_clients() then
+  -- Early return
+  if (not H.has_lsp_clients()) or H.lsp_request.status == 'sent' then
     if findstart == 1 then return -3 else return {} end
   end
 
-  -- Behave differently if called directly from user or as a result of a
-  -- LSP callback
-  if H.is_lsp_cache_empty() then
-    -- Initiate cache to indicate that request is currently in action
-    H.cache.lsp = {status = 'sent'}
+  -- NOTE: having code for request inside this function enables its use
+  -- directly with `<C-x><...>`.
+  if H.lsp_request.status ~= 'received' then
+    current_id = H.lsp_request.id + 1
+    H.lsp_request.id = current_id
+    H.lsp_request.status = 'sent'
 
     local bufnr = vim.api.nvim_get_current_buf()
     local params = vim.lsp.util.make_position_params()
@@ -98,47 +100,36 @@ function MiniCompletion.complete_lsp(findstart, base)
     -- of the line '  he', cursor position on the second call will be
     -- (<linenum>, 4) and line will be '  he' but on the second call -
     -- (<linenum>, 2) and '  ' (because 2 is a column of completion start).
-    -- This request is executed on first call because it returns `-3` on first
-    -- call (which means cancel and leave completion mode).
-    -- NOTE: using `buf_request_all()` to easily handle possible fallback and
-    -- to have all completion suggestions be filtered with one `base` in the
-    -- other route of this function. Anyway, the most common situation is with
-    -- one attached LSP client.
-    request_cancel = vim.lsp.buf_request_all(bufnr, 'textDocument/completion', params, function(result)
-      -- Empty LSP cache table or not Insert mode means that completion is
-      -- already not needed
-      if H.is_lsp_cache_empty() or vim.fn.mode() ~= 'i' then return end
+    -- This request is executed only on first call because it returns `-3` on
+    -- first call (which means cancel and leave completion mode).
+    -- NOTE: using `buf_request_all()` (instead of `buf_request()`) to easily
+    -- handle possible fallback and to have all completion suggestions be
+    -- filtered with one `base` in the other route of this function. Anyway,
+    -- the most common situation is with one attached LSP client.
+    cancel_fun = vim.lsp.buf_request_all(bufnr, 'textDocument/completion', params, function(result)
+      local is_current = H.lsp_request.id == current_id and H.lsp_request.status == 'sent'
+      if not is_current then return end
 
-      H.cache.lsp.status = 'received'
-      H.cache.lsp.result = result
+      H.lsp_request.status = 'received'
+      H.lsp_request.result = result
 
-      -- Trigger LSP completion to take another route
+      -- Trigger LSP completion to take 'received' route
       H.trigger_lsp()
     end)
 
     -- Cache cancel function to disable requests when they are not needed
-    H.cache.lsp.request_cancel = request_cancel
+    H.lsp_request.cancel_fun = cancel_fun
 
-    -- End completion and wait for LSP callbacks
+    -- End completion and wait for LSP callback
     if findstart == 1 then return -3 else return {} end
   else
-    -- Not empty LSP cache with `status` other than 'received' means that this
-    -- code is run not as a result of LSP request callback (but at the same
-    -- time there is an ongoing request). This can happen when some characters
-    -- were inserted after request was sent. In that case don't show any
-    -- completion and just wait for this code to be triggered from within
-    -- request callback (when `status` is not 'sent').
-    if H.cache.lsp.status ~= 'received' then
-      if findstart == 1 then return -3 else return {} end
-    end
-
     if findstart == 1 then return H.get_completion_start() end
 
-    local words = H.match_request_result(H.cache.lsp.result, base)
+    local words = H.match_request_result(H.lsp_request.result, base)
+    H.lsp_request.status = 'completed'
 
-    -- Clear cache and maybe trigger fallback action
-    H.cache.lsp = nil
-    if vim.tbl_isempty(words) and H.cache.do_fallback then
+    -- Maybe trigger fallback action
+    if vim.tbl_isempty(words) and H.do_fallback then
       H.trigger_fallback()
       return
     end
@@ -164,13 +155,20 @@ H.trigger_keys = {
 
 H.timers = {auto_complete = vim.loop.new_timer()}
 
-H.cache = {}
+-- Table describing current LSP request. Structure:
+-- - id: identifier (consecutive numbers).
+-- - status: status. One of 'sent', 'received', 'completed', 'canceled'.
+-- - result: result of request.
+-- - cancel_fun: function which cancels current request.
+H.lsp_request = {id = 0, status = nil, result = nil, cancel_fun = nil}
+
+H.do_fallback = true
 
 function H.trigger()
   if vim.fn.mode() ~= 'i' then return end
   if H.has_lsp_clients() then
     H.trigger_lsp()
-  elseif H.cache.do_fallback then
+  elseif H.do_fallback then
     H.trigger_fallback()
   end
 end
@@ -182,8 +180,6 @@ function H.trigger_lsp()
 end
 
 function H.trigger_fallback() H.feedkeys_in_insert(H.trigger_keys.ctrl_n) end
-
-function H.pumvisible() return vim.fn.pumvisible() ~= 0 end
 
 function H.has_lsp_clients() return not vim.tbl_isempty(vim.lsp.buf_get_clients()) end
 
@@ -202,13 +198,11 @@ function H.is_lsp_trigger(char)
   return vim.tbl_contains(triggers, char)
 end
 
-function H.is_lsp_cache_empty() return vim.tbl_isempty(H.cache.lsp or {}) end
-
-function H.cancel_lsp_requests()
-  if H.cache.lsp ~= nil and H.cache.lsp.request_cancel ~= nil then
-    H.cache.lsp.request_cancel()
+function H.cancel_lsp_request()
+  if vim.tbl_contains({'sent', 'received'}, H.lsp_request.status) then
+    if H.lsp_request.cancel_fun then H.lsp_request.cancel_fun() end
+    H.lsp_request.status = 'canceled'
   end
-  H.cache.lsp = nil
 end
 
 function H.feedkeys_in_insert(key)
@@ -216,7 +210,7 @@ function H.feedkeys_in_insert(key)
 end
 
 function H.get_completion_start()
-  -- Compute start position of latest prefix and prefix itself
+  -- Compute start position of latest keyword (as in `vim.lsp.omnifunc`)
   local pos = vim.api.nvim_win_get_cursor(0)
   local line = vim.api.nvim_get_current_line()
   local line_to_cursor = line:sub(1, pos[2])
