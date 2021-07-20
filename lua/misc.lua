ae334019e8b0318dd41e0d1ca71324b5ff033fa4
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2021-07-20 21:09:09 +0300

    Add 'bench_time()' miscellaneous function.

--- lua/misc.lua
+++ lua/misc.lua
@@ -2,3 +2,17 @@
 function _G.dump(x)
   print(vim.inspect(x))
 end
+
+-- Execute `f` once and time how long it took
+-- @param f Function which execution to benchmark
+-- @param ... Arguments when calling `f`
+-- @return duration, output Duration (in seconds; up to microseconds) and
+--   output of function execution
+function bench_time(f, ...)
+  local start_sec, start_usec = vim.loop.gettimeofday()
+  local output = f(...)
+  local end_sec, end_usec = vim.loop.gettimeofday()
+  local duration = (end_sec - start_sec) + 0.000001 * (end_usec - start_usec)
+
+  return duration, output
+end
