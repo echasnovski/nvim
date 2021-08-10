d87f359cf3fb5521a87a94658e1e372e886d8734
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2021-08-10 21:54:20 +0300

    Update 'Mint' colorscheme.

--- colors/mint.lua
+++ colors/mint.lua
@@ -1,33 +1,33 @@
 -- My personal "Mint" theme
--- Derived from base16 (https://github.com/chriskempson/base16) and
--- mini16 palette with background '#1f242e' and foreground '#e1f28c'
+-- Derived from base16 (https://github.com/chriskempson/base16) and mini16
+-- palette generator
+
+-- Dark palette is a 'mini16' with background '#29303d' (HSL = 220-20-20) and
+-- foreground '#e7f5a3' (HSL = 70-80-80)
 local palette = {
-  base00 = "#1f242e",
-  base01 = "#333c4c",
-  base02 = "#48546b",
-  base03 = "#5c6b89",
-  base04 = "#e1f28c",
-  base05 = "#d5ed5e",
-  base06 = "#c9e831",
-  base07 = "#b0ce17",
-  base08 = "#f29d8c",
-  base09 = "#ce3617",
-  base0A = "#aef28c",
-  base0B = "#54ce17",
-  base0C = "#8ce1f2",
-  base0D = "#17b0ce",
-  base0E = "#d08cf2",
-  base0F = "#9117ce"
+  base00 = "#29303d",
+  base01 = "#444f65",
+  base02 = "#5e6e8c",
+  base03 = "#8290ab",
+  base04 = "#e7f5a3",
+  base05 = "#dff283",
+  base06 = "#d6ee63",
+  base07 = "#ceeb42",
+  base08 = "#f5b1a3",
+  base09 = "#eb5e42",
+  base0A = "#bef5a3",
+  base0B = "#7aeb42",
+  base0C = "#a3e7f5",
+  base0D = "#42ceeb",
+  base0E = "#daa3f5",
+  base0F = "#b242eb"
 }
 
-require("mini.colors").base16(palette, 'Mint')
-vim.o.background = 'dark'
-
--- Brighten comments
-vim.cmd([[hi Comment guifg=#8693ac]])
+require('mini.colors').base16(palette, 'Mint')
 
 -- Make bright and bold color for operators and delimiters
-local bright_color = '#FFFFFF'
+local bright_color = '#ffffff'
+if vim.o.background == 'light' then bright_color = '#000000' end
 
 local hi_bright = function(group)
   vim.cmd(
