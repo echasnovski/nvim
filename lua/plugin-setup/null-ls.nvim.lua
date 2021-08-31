3061ee653bef871b02a84087982fc0058db3f12b
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2021-08-31 19:10:32 +0300

    Update 'null-ls' setup to use new built-in 'styler' for R formatting.

--- lua/plugin-setup/null-ls.nvim.lua
+++ lua/plugin-setup/null-ls.nvim.lua
@@ -4,35 +4,13 @@ if not (has_null_ls and has_lspconfig) then
   return
 end
 
-local helpers = require('null-ls.helpers')
-
--- Define 'styler' formatting for R. Package should be installed in library
--- used by `R` command.
-local r_styler = {
-  name = 'styler',
-  method = null_ls.methods.FORMATTING,
-  filetypes = { 'r', 'rmd' },
-  generator = helpers.formatter_factory({
-    command = 'R',
-    args = {
-      '--slave',
-      '--no-restore',
-      '--no-save',
-      '-e',
-      'con=file("stdin");output=styler::style_text(readLines(con));close(con);print(output, colored=FALSE)',
-    },
-    to_stdin = true,
-    suppress_errors = false,
-  }),
-}
-
-null_ls.register(r_styler)
-
 -- Configuring null-ls for other sources
 null_ls.config({
   sources = {
     -- `black` should be set up as callable from command line (be in '$PATH')
     null_ls.builtins.formatting.black,
+    -- 'styler' package should be installed in library used by `R` command.
+    null_ls.builtins.formatting.styler,
     -- `stylua` should be set up as callable from command line (be in '$PATH')
     null_ls.builtins.formatting.stylua,
   },
