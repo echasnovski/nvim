0523ea1e75b1fdaa7044828a362cc3f8c7dd0287
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2021-09-03 22:10:28 +0300

    Update 'mini.fuzzy'.
    Details:
    - Simplify algorithm's score (now uses only width and first match).
    - Export `MiniFuzzy.match()`.
    - Refactor previously exported functionality.

--- lua/mini/fuzzy.lua
+++ lua/mini/fuzzy.lua
@@ -1,50 +1,68 @@
 -- MIT License Copyright (c) 2021 Evgeni Chasnovski
 --
--- Lua module which implements functions fuzzy filtering and matching.
+-- Lua module which implements *minimal* and *fast* fuzzy matching.
 --
--- This module doesn't need to get activated. Call to `setup()` will create
--- global `MiniBase16` object. Use its functions as with normal Lua modules.
+-- This module doesn't need to get activated, but it can be done to
+-- improve usability. To activate, put this file somewhere into 'lua' folder
+-- and call module's `setup()`. For example, put as 'lua/mini/fuzzy.lua' and
+-- execute `require('mini.fuzzy').setup()` Lua code. It may have `config`
+-- argument which should be a table overwriting default values using same
+-- structure.
 --
--- Default `config`: {} (currently nothing to configure)
+-- Default `config`:
+-- {
+--   -- Maximum allowed value of match features (width and first match). All
+--   -- feature values greater than cutoff can be considered "equally bad".
+--   cutoff = 100
+-- }
 --
--- `fuzzy_match()` reduces a list of `cadidates` to those matching the
--- `word`.  By default it also sorts the output from best to worst matching.
--- Its design is mainly focused on `fuzzy typing completion` use case.
+-- Features:
+-- - `match(word, candidate)` tries to find best match for input string `word`
+--   (usually user input) and string `candidate`. Returns table with elements:
+--     - `positions` - list with letter indexes inside `candidate` which
+--       matched to corresponding letters in `word`. Or `nil` if no match.
+--     - `score` - positive number representing how good the match is (lower is
+--       better). Or `-1` if no match.
+-- - `filtersort(word, candidate_list)` filters `candidate_list` by leaving
+--   only those which matched with `word` and sorts from best to worst matches
+--   (based on score and index in original list, both lower is better).
+-- - `process_lsp_items(items, base)` implements fuzzy matching for
+--   `MiniCompletion.lsp_completion.process_items`.
+-- - `get_telescope_sorter(opts)` implements a getter for 'telescope.nvim'
+--   sorter. Designed to be used exactly as built-in sorters (like
+--   `get_generic_fuzzy_sorter`).
 --
--- *Algorithm*
--- Current approach is a slightly modified and simplified version of FZF's fast
--- (version 1) algorithm. It is mainly designed for speed sacrificing small
--- amount of match precision. Core ideas:
--- - Word and candidate are matched only if all of word's letters appear in
---   candidate in the same order. The exact matching of word's letters to
---   candidate ones is done in two steps:
---     - Find word's first letter starting from candidate's left and looking
---       forward. Then find word's second letter after the first match, and so
---       on. Candidate is matched if word's last letter is found.
---     - In case of a match, improve it: starting from candidate's match for
---       word's last letter, find word's second to last letter looking
---       backward, and so on. This step tries to improve match for cases like
---       `word='time', candidate='get_time'`: without it word's 't' will be
---       matched with candidate's third letter when in practice fifth letter
---       is more appropriate.
--- - Sorting is done based on comparing candidate's matching of word's
---   letters. It has the following logic (with examples for `word = 'time'`):
---     - Tighter match is more favourable (measured by 'index of last letter'
---       minus 'index of first letter'). So '_time' is a better match than
---       't_ime'. NOTE: due to the fact that currently only one "match
---       improvement" is done, this rule might not always hold. For example,
---       't__ime__time' is a worse match than `t_ime`, despite the fact that
---       it contains an exact word (which didn't end up being matched).
---     - In case of equally tight matches, the one starting earlier is
---       better. So 'time_a' is better than 'a_time'.
---     - In case of equally tight and early matches, the one having more
---       letters matched earlier is better. This is measured by the sum of
---       matched letter indexes. So 'tim_e' is better than 't_ime'.
---     - If previous steps can't decide a better candidate, the one with
---       earlier table index is better. This preserves input ordering of
---       candidates.
+-- NOTEs:
+-- - Currently there is no explicit design to work with multibyte symbols, but
+--   simple examples should work.
+-- - Smart case is used: case insensitive if input word (which is usually a
+--   user input) is all lower ase. Case sensitive otherwise.
 --
--- NOTE: currently doesn't work with multibyte symbols.
+-- *Algorithm design*
+-- General design uses only width of found match and index of first letter
+-- match. No special characters or positions (like in fzy and fzf) are used.
+--
+-- Given input `word` and target `candidate`:
+-- - The goal is to find matching between `word`'s letters and letters in
+--   `candidate`, which minimizes certain score. It is assumed that order of
+--   letters in `word` and those matched in `candidate` should be the same.
+-- - Matching is represented by matched positions: a list `positions` of
+--   integers with length equal to number of letter in `word`. The following
+--   should be always true in case of a match: `candidate`'s letter at index
+--   `positions[i]` is letters[i]` for all valid `i`.
+-- - Matched positions are evaluated based only on two features: their width
+--   (number of indexes between first and last positions) and first match
+--   (index of first letter match). There is a global setting `cutoff` for
+--   which all feature values greater than it can be considered "equally bad".
+-- - Score of matched positions is computed with following explicit formula:
+--   `cutoff * min(width, cutoff) + min(first, cutoff)`. It is designed to be
+--   equivalent to first comparing widths (lower is better) and then comparing
+--   first match (lower is better). For example, if `word = 'time'`:
+--     - '_time' (width 4) will have a better match than 't_ime' (width 5).
+--     - 'time_a' (width 4, first 1) will have a better match than 'a_time'
+--       (width 4, first 3).
+-- - Final matched positions are those which minimize score among all possible
+--   matched positions of `word` and `candidate`.
 
 -- Module and its helper
 local MiniFuzzy = {}
@@ -62,27 +80,42 @@ function MiniFuzzy.setup(config)
   H.apply_config(config)
 end
 
--- @param candidates Lua array of strings inside which word will be searched
--- @param word String which will be searched
--- @param sort (default: `true`) Whether to sort output candidates from best
---   to worst match
--- @param case_sensitive (default: `false`) Whether search is case sensitive.
--- @return matched_candidates, matched_indexes Arrays of matched candidates
---   and their indexes in original input.
-function MiniFuzzy.fuzzy_match(candidates, word, sort, case_sensitive)
-  sort = sort or true
-  case_sensitive = case_sensitive or false
+-- Module settings
+---- Maximum allowed value of match features (width and first match). All
+---- feature values greater than cutoff can be considered "equally bad".
+MiniFuzzy.cutoff = 100
 
-  local matches = H.fuzzy_filter_impl(word, candidates, case_sensitive)
-  if sort then
-    table.sort(matches, H.fuzzy_compare)
+-- Module functionality
+---- Compute match data of input `word` and `candidate` strings
+function MiniFuzzy.match(word, candidate)
+  -- Use 'smart case'
+  candidate = (word == word:lower()) and candidate:lower() or candidate
+
+  local positions = H.find_best_positions(H.string_to_letters(word), candidate)
+  return { positions = positions, score = H.score_positions(positions) }
+end
+
+-- @param word String which will be searched
+-- @param candidate_list Lua list of strings inside which word will be searched
+-- @return matched_candidates, matched_indexes Arrays of matched candidates and
+--   their indexes in original input.
+function MiniFuzzy.filtersort(word, candidate_list)
+  -- Use 'smart case'. New list is needed to preserve input for later filtering
+  local cand_list
+  if word ~= word:lower() then
+    cand_list = vim.tbl_map(string.lower, candidate_list)
+  else
+    cand_list = candidate_list
   end
 
-  return H.matches_to_tuple(matches)
+  local filter_ids = H.make_filter_indexes(word, cand_list)
+  table.sort(filter_ids, H.compare_filter_indexes)
+
+  return H.filter_by_indexes(candidate_list, filter_ids)
 end
 
 ---- Fuzzy matching for `MiniCompletion.lsp_completion.process_items`
-function MiniFuzzy.fuzzy_process_lsp_items(items, base, sort, case_sensitive)
+function MiniFuzzy.process_lsp_items(items, base)
   -- Extract completion words from items
   local words = vim.tbl_map(function(x)
     if type(x.textEdit) == 'table' and x.textEdit.newText then
@@ -92,27 +125,23 @@ function MiniFuzzy.fuzzy_process_lsp_items(items, base, sort, case_sensitive)
   end, items)
 
   -- Fuzzy match
-  local _, match_inds = MiniFuzzy.fuzzy_match(words, base, sort, case_sensitive)
+  local _, match_inds = MiniFuzzy.filtersort(base, words)
   return vim.tbl_map(function(i)
     return items[i]
   end, match_inds)
 end
 
+---- Custom getter for sorter for 'telescope.nvim'
 function MiniFuzzy.get_telescope_sorter(opts)
-  opts = vim.tbl_deep_extend('force', { case_sensitive = false }, opts or {})
+  opts = opts or {}
 
   return require('telescope.sorters').Sorter:new({
     start = function(self, prompt)
       -- Cache prompt's letters
-      local letters = {}
-      if not opts.case_sensitive then
-        prompt = prompt:lower()
-      end
-      for i = 1, #prompt do
-        -- Use `vim.pesc()` to treat special characters ('.', etc.) literally
-        letters[i] = vim.pesc(prompt:sub(i, i))
-      end
-      self.letters = letters
+      self.letters = H.string_to_letters(prompt)
+
+      -- Use 'smart case': insensitive if `prompt` is lowercase
+      self.case_sensitive = prompt ~= prompt:lower()
     end,
 
     -- @param self
@@ -120,8 +149,12 @@ function MiniFuzzy.get_telescope_sorter(opts)
     -- @param line (entry.ordinal)
     -- @param entry (the whole entry)
     scoring_function = function(self, _, line, _)
-      local match = H.match(self.letters, line, opts.case_sensitive)
-      return H.match_to_score(match)
+      if #self.letters == 0 then
+        return 1
+      end
+      line = self.case_sensitive and line or line:lower()
+      local positions = H.find_best_positions(self.letters, line)
+      return H.score_positions(positions)
     end,
 
     -- Currently there doesn't seem to be a proper way to cache matched
@@ -132,11 +165,17 @@ function MiniFuzzy.get_telescope_sorter(opts)
       if #self.letters == 0 or #display == 0 then
         return {}
       end
-      local match = H.match(self.letters, display, opts.case_sensitive)
-      return match.positions
+      display = self.case_sensitive and display or display:lower()
+      return H.find_best_positions(self.letters, display)
     end,
   })
 end
+
+-- Helper data
+---- Module default config
+H.config = {
+  cutoff = MiniFuzzy.cutoff,
+}
 
 -- Helper functions
 ---- Settings
@@ -146,55 +185,42 @@ function H.setup_config(config)
   vim.validate({ config = { config, 'table', true } })
   config = vim.tbl_deep_extend('force', H.config, config or {})
 
+  vim.validate({
+    cutoff = {
+      config.cutoff,
+      function(x)
+        return type(x) == 'number' and x >= 1
+      end,
+      'number not less than 1',
+    },
+  })
+
   return config
 end
 
-function H.apply_config(config) end
-
----- Fuzzy matching
-function H.fuzzy_filter_impl(word, candidates, case_sensitive)
-  -- Precompute a table of word's letters
-  local n_word = #word
-  if not case_sensitive then
-    word = word:lower()
-  end
-  local letters = {}
-  for i = 1, n_word do
-    -- Use `vim.pesc()` to treat special characters ('.', etc.) literally
-    letters[i] = vim.pesc(word:sub(i, i))
-  end
-
-  local res = {}
-  for i, cand in ipairs(candidates) do
-    local match = H.match(letters, cand, case_sensitive)
-    if match then
-      match.candidate, match.index = cand, i
-      match.score = H.match_to_score(match)
-      table.insert(res, match)
-    end
-  end
-
-  return res
+function H.apply_config(config)
+  MiniFuzzy.cutoff = config.cutoff
 end
 
--- @param letters List of letters from "typed" word
+---- Fuzzy matching
+-- @param letters List of letters from input word
 -- @param candidate String of interest
--- @param case_sensitive Whether match is case sensitive
 --
--- @return Table with match information if there is a match, `nil` otherwise.
-function H.match(letters, candidate, case_sensitive)
+-- @return Table with matched positions (in `candidate`) if there is a match,
+--   `nil` otherwise.
+function H.find_best_positions(letters, candidate)
   local n_candidate, n_letters = #candidate, #letters
-  if n_candidate <= n_letters then
+  if n_letters == 0 or n_candidate < n_letters then
     return nil
   end
 
-  local cand = case_sensitive and candidate or candidate:lower()
-
-  -- Make forward search for match presence
-  local pos_last, let_i = 0, 1
-  while pos_last and let_i <= n_letters do
-    pos_last = string.find(cand, letters[let_i], pos_last + 1)
-    let_i = let_i + 1
+  -- Search forward to find matching positions with left-most last letter match
+  local pos_last = 0
+  for let_i = 1, #letters do
+    pos_last = candidate:find(letters[let_i], pos_last + 1)
+    if not pos_last then
+      break
+    end
   end
 
   -- Candidate is matched only if word's last letter is found
@@ -202,52 +228,95 @@ function H.match(letters, candidate, case_sensitive)
     return nil
   end
 
-  -- Compute matching positions by going backwards from last letter match
+  -- If there is only one letter, it is already the best match (there will not
+  -- be better width and it has lowest first match)
   if n_letters == 1 then
-    return { positions = { pos_last }, pos_width = 1, pos_first = pos_last, pos_mean = pos_last }
+    return { pos_last }
   end
 
-  local rev_cand, rev_last = cand:reverse(), n_candidate - pos_last + 1
+  -- Compute best match positions by iteratively checking all possible last
+  -- letter matches (at and after initial one). At end of each iteration
+  -- `best_pos_last` holds best match for last letter among all previously
+  -- checked such matches.
+  local best_pos_last, best_width = pos_last, math.huge
+  local rev_candidate = candidate:reverse()
 
-  local positions, pos_sum = { pos_last }, pos_last
-  local rev_pos = rev_last
+  while pos_last do
+    -- Simulate computing best match positions ending exactly at `pos_last` by
+    -- going backwards from current last letter match. This works because it
+    -- minimizes width which is the only way to find match with lower score.
+    -- Not actually creating table with positions and then directly computing
+    -- score increases speed by up to 40% (on small frequent input word with
+    -- relatively wide candidate, such as file paths of nested directories).
+    local rev_first = n_candidate - pos_last + 1
+    for i = #letters - 1, 1, -1 do
+      rev_first = rev_candidate:find(letters[i], rev_first + 1)
+    end
+    local first = n_candidate - rev_first + 1
+    local width = math.min(pos_last - first + 1, MiniFuzzy.cutoff)
+
+    -- Using strict sign is crucial because when two last letter matches result
+    -- into positions with similar width, the one which was created earlier
+    -- (i.e. with smaller last letter match) will have smaller first letter
+    -- match (hence better score).
+    if width < best_width then
+      best_pos_last, best_width = pos_last, width
+    end
+
+    -- Advance iteration
+    pos_last = candidate:find(letters[n_letters], pos_last + 1)
+  end
+
+  -- Actually compute best matched positions from best last letter match
+  local best_positions = { best_pos_last }
+  local rev_pos = n_candidate - best_pos_last + 1
   for i = #letters - 1, 1, -1 do
-    rev_pos = rev_cand:find(letters[i], rev_pos + 1)
-    local pos = n_candidate - rev_pos + 1
-    table.insert(positions, pos)
-    pos_sum = pos_sum + pos
+    rev_pos = rev_candidate:find(letters[i], rev_pos + 1)
+    -- For relatively small number of letters (around 10, which is main use
+    -- case) inserting to front seems to have better performance than
+    -- inserting at end and then reversing.
+    table.insert(best_positions, 1, n_candidate - rev_pos + 1)
   end
-  local pos_first = n_candidate - rev_pos + 1
 
-  return {
-    positions = H.tbl_reverse(positions),
-    pos_width = pos_last - pos_first + 1,
-    pos_first = pos_first,
-    pos_mean = pos_sum / n_letters,
-  }
+  return best_positions
 end
 
--- Convert match information into score. Smaller values indicate better match
+-- Compute score of matched positions. Smaller values indicate better match
 -- (i.e. like distance). Reasoning behind the score is for it to produce the
--- same ordering as with sequential comparison of match's width, first position
--- and mean position. So it shouldn't be perceived as linear distance
--- (difference between scores don't really matter, only their comparison with
--- each other).
+-- same ordering as with sequential comparison of match's width and first
+-- position. So it shouldn't really be perceived as linear distance (difference
+-- between scores don't really matter, only their comparison with each other).
 --
 -- Reasoning behind comparison logic (based on 'time' input):
 -- - '_time' is better than 't_ime' (width is smaller).
--- - 'time_aa' is better than 'aa_time' (width is same, first position is
---   smaller).
--- - 'tim_e' is better than 't_ime' (width and first position are same, mean
---   position is smaller).
-function H.match_to_score(match)
-  if not match then return -1 end
-  return 1000 * math.min(match.pos_width, 1000)
-    + 1 * math.min(match.pos_first, 1000)
-    + 0.001 * math.min(match.pos_mean, 1000)
+-- - 'time_aa' is better than 'aa_time' (same width, first match is smaller).
+--
+-- Returns -1 if `positions` is `nil` or empty.
+function H.score_positions(positions)
+  if not positions or #positions == 0 then
+    return -1
+  end
+  local first, last = positions[1], positions[#positions]
+  local cutoff = MiniFuzzy.cutoff
+  return cutoff * math.min(last - first + 1, cutoff) + math.min(first, cutoff)
 end
 
-function H.fuzzy_compare(a, b)
+function H.make_filter_indexes(word, candidate_list)
+  -- Precompute a table of word's letters
+  local letters = H.string_to_letters(word)
+
+  local res = {}
+  for i, cand in ipairs(candidate_list) do
+    local positions = H.find_best_positions(letters, cand)
+    if positions then
+      table.insert(res, {index = i, score = H.score_positions(positions)})
+    end
+  end
+
+  return res
+end
+
+function H.compare_filter_indexes(a, b)
   if a.score < b.score then
     return true
   end
@@ -260,25 +329,19 @@ function H.fuzzy_compare(a, b)
   return false
 end
 
-function H.matches_to_tuple(matches)
-  local candidates, indexes = {}, {}
-  for _, m in pairs(matches) do
-    table.insert(candidates, m.candidate)
-    table.insert(indexes, m.index)
+function H.filter_by_indexes(candidate_list, ids)
+  local res, res_ids = {}, {}
+  for _, id in pairs(ids) do
+    table.insert(res, candidate_list[id.index])
+    table.insert(res_ids, id.index)
   end
 
-  return candidates, indexes
+  return res, res_ids
 end
 
-function H.tbl_reverse(t)
-  if #t == 0 then
-    return {}
-  end
-  local res = {}
-  for i = #t, 1, -1 do
-    table.insert(res, t[i])
-  end
-  return res
+---- Utilities
+function H.string_to_letters(s)
+  return vim.tbl_map(vim.pesc, vim.split(s, ''))
 end
 
 return MiniFuzzy
