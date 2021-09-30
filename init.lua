0bf85b21be36448bc7794f3e4250ae63ce1e5515
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2021-09-30 21:12:34 +0300

    Stop using 'init.vim' in favor of 'init.lua'.
    Details:
    - Thank you for your service!

---
+++ init.lua
@@ -0,0 +1,15 @@
+require('ec.settings')
+require('ec.functions')
+require('ec.mappings')
+require('ec.mappings-leader')
+
+if vim.fn.exists('g:vscode') == 1 then
+  -- Configuration for integration with VS Code
+  require('ec.vscode')
+else
+  -- Source internal 'mini' plugins
+  require('mini')
+end
+
+-- Everything that should be configured at the end
+require('ec.zzz')
--- lua/source.lua
+++
@@ -1,7 +0,0 @@
--- Entry point from 'init.vim' to source all Lua files
-
--- Source internal 'mini' plugins
-require('mini')
-
--- Source everything that should be sourced last
-require('zzz')
