3ff6f392fdff074e83dd9be4ae2369f3254e6530
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2021-10-17 16:43:44 +0300

    Update 'mini.statusline' config to show 'spell' and 'wrap'.

--- lua/ec/configs/mini.lua
+++ lua/ec/configs/mini.lua
@@ -1,4 +1,32 @@
-require('mini.statusline').setup()
+require('mini.statusline').setup({
+  content = {
+    active = function()
+      -- stylua: ignore start
+      local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+      local spell         = vim.wo.spell and (MiniStatusline.is_truncated(120) and 'S' or 'SPELL') or ''
+      local wrap          = vim.wo.wrap  and (MiniStatusline.is_truncated(120) and 'W' or 'WRAP')  or ''
+      local git           = MiniStatusline.section_git({ trunc_width = 75 })
+      local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+      local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
+      local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+      local location      = MiniStatusline.section_location({ trunc_width = 75 })
+
+      -- Usage of `MiniStatusline.combine_groups()` ensures highlighting and
+      -- correct padding with spaces between groups (accounts for 'missing'
+      -- sections, etc.)
+      return MiniStatusline.combine_groups({
+        { hl = mode_hl,                  strings = { mode, spell, wrap } },
+        { hl = 'MiniStatuslineDevinfo',  strings = { git, diagnostics } },
+        '%<', -- Mark general truncate point
+        { hl = 'MiniStatuslineFilename', strings = { filename } },
+        '%=', -- End left alignment
+        { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+        { hl = mode_hl,                  strings = { location } },
+      })
+      -- stylua: ignore end
+    end,
+  },
+})
 require('mini.tabline').setup()
 
 vim.defer_fn(function()
