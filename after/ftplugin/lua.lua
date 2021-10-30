7b2162b1e2c52e11f122c19fb804a1333dc486fd
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2021-10-30 13:09:09 +0300

    Update StyLua integration.

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,4 +1,6 @@
 -- Use custom comment leaders to allow both nested variants (`--` and `----`)
 -- and "docgen" variant (`---`).
 -- Using `defer_fn` to ensure this is executed after all possible autocommands.
-vim.defer_fn(function() vim.opt.comments = ':----,:---,:--' end, 0)
+vim.defer_fn(function()
+  vim.opt.comments = ':----,:---,:--'
+end, 0)
