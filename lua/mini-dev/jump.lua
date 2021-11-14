4503015207a0069bd8d782c5c463afa728d30b2d
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2021-11-14 18:11:12 +0200

    Update 'mini.jump'.
    Details:
    - Add `n_times` argument to `MiniJump.jump()`.
    - Make Operator-pending mode an expression mapping to allow full
      dot-repeat.

--- lua/mini-dev/jump.lua
+++ lua/mini-dev/jump.lua
@@ -9,8 +9,8 @@
 --- - Repeat jump by pressing f, F, t, T again. It is reset when cursor moved
 ---   as a result of not jumping.
 --- - Highlight (after customizable delay) of all possible target characters.
---- - Normal, visual, and operator-pending (with a reasonable dot-repeat) modes
----   are supported.
+--- - Normal, Visual, and Operator-pending (with full dot-repeat) modes are
+---   supported.
 ---
 --- # Setup
 ---
@@ -90,23 +90,18 @@ MiniJump.config = {
 --- Takes a string and jumps to the first occurrence of it after the cursor.
 ---
 --- @param target string: The string to jump to.
---- @param backward boolean: Whether to jump backward. Default: latest used value.
---- @param till boolean: Whether to jump just before/after the match instead of exactly on target. Also ignore matches that don't have anything before/after them. Default: latest used value.
-function MiniJump.jump(target, backward, till)
+--- @param backward boolean: Whether to jump backward. Default: latest used value or `false`.
+--- @param till boolean: Whether to jump just before/after the match instead of exactly on target. Also ignore matches that don't have anything before/after them. Default: latest used value or `false`.
+--- @param n_times number: Number of times to perform a jump. Default: latest used value or 1.
+function MiniJump.jump(target, backward, till, n_times)
   if H.is_disabled() then
     return
   end
 
-  H.cache.mode = vim.fn.mode(1)
-  H.cache.target = target == nil and H.cache.target or target
-  -- Don't use `? and <1> or <2>` because it doesn't work when `<1>` is `false`
-  if backward ~= nil then
-    H.cache.backward = backward
-  end
-  if till ~= nil then
-    H.cache.till = till
-  end
+  -- Cache inputs for future use
+  H.update_cache(target, backward, till, n_times)
 
+  -- Construct search and highlight patterns
   local flags = H.cache.backward and 'Wb' or 'W'
   local pattern, hl_pattern = [[\V%s]], [[\V%s]]
   if H.cache.till then
@@ -118,8 +113,8 @@ function MiniJump.jump(target, backward, till)
     end
   end
 
-  target = vim.fn.escape(H.cache.target, [[\]])
-  pattern, hl_pattern = pattern:format(target), hl_pattern:format(target)
+  local escaped_target = vim.fn.escape(H.cache.target, [[\]])
+  pattern, hl_pattern = pattern:format(escaped_target), hl_pattern:format(escaped_target)
 
   -- Delay highlighting after stopping previous one
   H.timer:stop()
@@ -131,46 +126,64 @@ function MiniJump.jump(target, backward, till)
     end)
   )
 
-  -- Make jump
+  -- Make jump(s)
   H.n_cursor_moved = 0
   H.jumping = true
-  vim.fn.search(pattern, flags)
+  for _ = 1, H.cache.n_times do
+    vim.fn.search(pattern, flags)
+  end
 
   -- Open enough folds to show jump
   vim.cmd([[normal! zv]])
 end
 
---- Smart jump
+--- Make smart jump
 ---
 --- If the last movement was a jump, perform another jump with the same target.
 --- Otherwise, wait for a target input (via |getchar()|). Respects |v:count|.
 ---
---- @param backward boolean: Whether to jump backward. Default: latest used value.
---- @param till boolean: Whether to jump just before/after the match instead of exactly on target. Also ignore matches that don't have anything before/after them. Default: latest used value.
+--- @param backward boolean: Whether to jump backward. Default: latest used value or `false`.
+--- @param till boolean: Whether to jump just before/after the match instead of exactly on target. Also ignore matches that don't have anything before/after them. Default: latest used value or `false`.
 function MiniJump.smart_jump(backward, till)
   if H.is_disabled() then
     return
   end
 
-  -- Keep track of *full* mode (tracks operator-pending case)
+  -- Jumping should stop after mode change. Use `mode(1)` to track 'omap' case.
   local cur_mode = vim.fn.mode(1)
   if H.cache.mode ~= cur_mode then
     MiniJump.stop_jumping()
   end
 
+  -- Ask for target only when needed
+  local target
   if not H.jumping or H.cache.target == nil then
-    local char = vim.fn.getchar()
-    -- Allow `<Esc>` to early exit
-    if char == 27 then
-      return
-    end
-
-    H.cache.target = vim.fn.nr2char(char)
+    target = H.get_target()
   end
 
-  for _ = 1, vim.v.count1 do
-    MiniJump.jump(H.cache.target, backward, till)
+  H.update_cache(target, backward, till, vim.v.count1)
+
+  MiniJump.jump()
+end
+
+--- Make expression jump
+---
+--- Cache information about the jump and return string with command to perform
+--- jump. Designed to be used inside Operator-pending mapping (see
+--- |omap-info|). Always asks for target (via |getchar()|). Respects |v:count|.
+---
+--- @param backward boolean: Whether to jump backward. Default: latest used value or `false`.
+--- @param till boolean: Whether to jump just before/after the match instead of exactly on target. Also ignore matches that don't have anything before/after them. Default: latest used value or `false`.
+function MiniJump.expr_jump(backward, till)
+  if H.is_disabled() then
+    return ''
   end
+
+  -- Always ask for `target` as this will be used only in operator-pending
+  -- mode. Dot-repeat will be implemented via expression-mapping.
+  H.update_cache(H.get_target(), backward, till, vim.v.count1)
+
+  return vim.api.nvim_replace_termcodes('v:<C-u>lua MiniJump.jump()<CR>', true, true, true)
 end
 
 --- Stop jumping
@@ -200,7 +213,7 @@ end
 H.default_config = MiniJump.config
 
 -- Cache for the latest jump
-H.cache = { mode = nil, target = nil, backward = false, till = false }
+H.cache = { mode = nil, target = nil, backward = false, till = false, n_times = 1 }
 
 -- Indicator of whether inside smart jumping
 H.jumping = false
@@ -241,11 +254,20 @@ end
 function H.apply_config(config)
   MiniJump.config = config
 
-  local modes = { 'n', 'o', 'x' }
-  H.map_cmd(modes, config.mappings.forward, [[lua MiniJump.smart_jump(false, false)]])
-  H.map_cmd(modes, config.mappings.backward, [[lua MiniJump.smart_jump(true, false)]])
-  H.map_cmd(modes, config.mappings.forward_till, [[lua MiniJump.smart_jump(false, true)]])
-  H.map_cmd(modes, config.mappings.backward_till, [[lua MiniJump.smart_jump(true, true)]])
+  H.map('n', config.mappings.forward, [[<Cmd>lua MiniJump.smart_jump(false, false)<CR>]])
+  H.map('n', config.mappings.backward, [[<Cmd>lua MiniJump.smart_jump(true, false)<CR>]])
+  H.map('n', config.mappings.forward_till, [[<Cmd>lua MiniJump.smart_jump(false, true)<CR>]])
+  H.map('n', config.mappings.backward_till, [[<Cmd>lua MiniJump.smart_jump(true, true)<CR>]])
+
+  H.map('x', config.mappings.forward, [[<Cmd>lua MiniJump.smart_jump(false, false)<CR>]])
+  H.map('x', config.mappings.backward, [[<Cmd>lua MiniJump.smart_jump(true, false)<CR>]])
+  H.map('x', config.mappings.forward_till, [[<Cmd>lua MiniJump.smart_jump(false, true)<CR>]])
+  H.map('x', config.mappings.backward_till, [[<Cmd>lua MiniJump.smart_jump(true, true)<CR>]])
+
+  H.map('o', config.mappings.forward, [[v:lua.MiniJump.expr_jump(v:false, v:false)]], { expr = true })
+  H.map('o', config.mappings.backward, [[v:lua.MiniJump.expr_jump(v:true, v:false)]], { expr = true })
+  H.map('o', config.mappings.forward_till, [[v:lua.MiniJump.expr_jump(v:false, v:true)]], { expr = true })
+  H.map('o', config.mappings.backward_till, [[v:lua.MiniJump.expr_jump(v:true, v:true)]], { expr = true })
 end
 
 function H.is_disabled()
@@ -286,15 +308,41 @@ function H.unhighlight()
 end
 
 -- Various helpers --
-function H.map_cmd(modes, key, command)
+function H.update_cache(target, backward, till, n_times)
+  H.cache.mode = vim.fn.mode(1)
+
+  -- Don't use `? and <1> or <2>` because it doesn't work when `<1>` is `false`
+  if target ~= nil then
+    H.cache.target = target
+  end
+  if backward ~= nil then
+    H.cache.backward = backward
+  end
+  if till ~= nil then
+    H.cache.till = till
+  end
+  if n_times ~= nil then
+    H.cache.n_times = n_times
+  end
+end
+
+function H.get_target()
+  local char = vim.fn.getchar()
+  -- Allow `<Esc>` to early exit
+  if char == 27 then
+    return
+  end
+
+  return vim.fn.nr2char(char)
+end
+
+function H.map(mode, key, rhs, opts)
   if key == '' then
     return
   end
-  for _, mode in ipairs(modes) do
-    local prefix = mode == 'o' and 'v' or ''
-    local rhs = ('%s<cmd>%s<cr>'):format(prefix, command)
-    vim.api.nvim_set_keymap(mode, key, rhs, { noremap = true })
-  end
+
+  opts = vim.tbl_deep_extend('force', { noremap = true }, opts or {})
+  vim.api.nvim_set_keymap(mode, key, rhs, opts)
 end
 
 return MiniJump
