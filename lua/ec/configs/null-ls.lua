7187513e3396c744b50dbacb8f4c200d1e406c84
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2021-12-26 17:44:39 +0200

    Update plugins.

--- lua/ec/configs/null-ls.lua
+++ lua/ec/configs/null-ls.lua
@@ -1,11 +1,10 @@
-local has_null_ls, null_ls = pcall(require, 'null-ls')
-local has_lspconfig, lspconfig = pcall(require, 'lspconfig')
-if not (has_null_ls and has_lspconfig) then
-  return
-end
+local null_ls = require('null-ls')
 
--- Configuring null-ls for other sources
-null_ls.config({
+-- Set up null-ls server
+-- NOTE: currently mappings for formatting with `vim.lsp.buf.formatting()` and
+-- `vim.lsp.buf.range_formatting()` are set up for every buffer in
+-- 'mappings-leader.vim'. This is done to make 'which-key' respect labels.
+null_ls.setup({
   sources = {
     -- `black` should be set up as callable from command line (be in '$PATH')
     null_ls.builtins.formatting.black,
@@ -17,9 +16,3 @@ null_ls.config({
     null_ls.builtins.formatting.stylua,
   },
 })
-
--- Set up null-ls server
--- NOTE: currently mappings for formatting with `vim.lsp.buf.formatting()` and
--- `vim.lsp.buf.range_formatting()` are set up for every buffer in
--- 'mappings-leader.vim'. This is done to make 'which-key' respect labels.
-lspconfig['null-ls'].setup({})
