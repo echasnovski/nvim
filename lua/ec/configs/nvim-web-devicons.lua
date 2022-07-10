7baf40c294bbec1556ccd2aa0176417cd4f263e2
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2022-07-10 10:40:33 +0300

    Update plugins.

---
+++ lua/ec/configs/nvim-web-devicons.lua
@@ -0,0 +1,17 @@
+require('nvim-web-devicons').setup({
+  override = {
+    ['md'] = {
+      icon = '',
+      color = '#ffffff',
+      cterm_color = 'white',
+      name = 'Md',
+    },
+    ['mdx'] = {
+      icon = '',
+      color = '#519aba',
+      cterm_color = '67',
+      name = 'Mdx',
+    },
+  },
+  default = true,
+})
