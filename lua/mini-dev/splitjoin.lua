a2f278a40d058ed299d8f1f4117c4b43df235916
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2023-03-02 11:01:41 +0200

    Update 'mini.splitjoin'.

--- lua/mini-dev/splitjoin.lua
+++ lua/mini-dev/splitjoin.lua
@@ -3,24 +3,15 @@
 -- TODO:
 --
 -- Code:
--- - Decide if it is worth doing Lua pattern-based approach or
---   `searchpairpos()` one. First one is **very** fast, second one is slow on
---   big buffers with many false (not balanced) matches. The second one has
---   more compact implementation and can be used to ignore matches in strings
---   (but *slow*).
--- - Split from end to start. This should not require extmarks for determining
---   where to split.
+-- - Make sure splitting doesn't trigger `InsertEnter` and `InsertLeave`.
 -- - Design:
 --     - Ensure correct indentation logic:
 --         - Split in such a way that preserve indent of previous line.
 --         - Indent all lines after first split and before last split.
---     - `split(opts)` and `join(opts)` should be forced variant of toggle.
---       One of main use cases for `split()` is to update linewise addition of
---       surrounding to be on separate lines with indented inner ones.
---     - `split_at(positions)` and `join_at(from_line, to_line)` are low-level split and join.
 -- - Features:
 --     - Ensure that it works both inside strings and comments.
 --     - Ensure it works on empty brackets.
+-- - Polish.
 --
 -- Tests:
 --
@@ -64,6 +55,7 @@
 ---@tag MiniSplitjoin
 
 ---@diagnostic disable:undefined-field
+---@diagnostic disable:discard-returns
 
 -- Module definition ==========================================================
 MiniSplitjoin = {}
@@ -92,8 +84,9 @@ end
 ---@text Options ~
 MiniSplitjoin.config = {
   -- TODO: replace with `nil`
-  brackets = { '()', '[]', '{}' },
-  separators = { ',' },
+  brackets = { '%b()', '%b[]', '%b{}' },
+  separator = ',',
+  exclude_regions = { '%b""', "%b''", '%b()', '%b[]', '%b{}' },
 
   -- Do you want this as a way to manage all nuances?
   -- Like "pad braces"
@@ -110,69 +103,88 @@ MiniSplitjoin.config = {
 }
 --minidoc_afterlines_end
 
-MiniSplitjoin.toggle = function(opts)
-  -- If ends are on different lines - join the lines.
-  -- If ends are on same line:
-  -- - Parse "arguments" (line parts separated by delimiter pattern).
-  -- - Put each one on new line (accounting for indent and 'commentstring').
-  opts = vim.tbl_deep_extend('force', H.get_config(), opts or {})
-  opts.brackets = opts.brackets or { '()', '[]', '{}' }
-  opts.separators = opts.separators or { ',' }
+MiniSplitjoin.toggle = function(pos, opts)
+  pos = pos or vim.api.nvim_win_get_cursor(0)
+  opts = H.get_opts(opts)
 
-  -- Find smallest surrounding brackets (`()`, `[]`, `{}`) on cursor position.
-  local region = H.find_smallest_bracket_region(opts.brackets)
+  local region = H.find_smallest_bracket_region(pos, opts.brackets)
   if region == nil then return end
 
   if region.from.line == region.to.line then
     -- Split
+    local positions = H.find_split_positions(region, opts.separator, opts.exclude_regions)
+    MiniSplitjoin.split_at(positions)
   else
     -- Join
-    MiniSplitjoin.join(region.from.line, region.to.line)
+    MiniSplitjoin.join_at(region.from.line, region.to.lie)
   end
 end
 
-MiniSplitjoin.split = function(line, cols)
-  -- Put extmarks at split positions and cursor
-  local split_extmarks = {}
-  for i, col in ipairs(cols) do
-    split_extmarks[i] = vim.api.nvim_buf_set_extmark(0, H.ns_id, line - 1, col - 1, {})
-  end
+MiniSplitjoin.split = function(pos, opts)
+  pos = pos or vim.api.nvim_win_get_cursor(0)
+  opts = H.get_opts(opts)
+
+  local region = H.find_smallest_bracket_region(pos, opts.brackets)
+  if region == nil then return end
+
+  local positions = H.find_split_positions(region, opts.separator, opts.exclude_regions)
+  MiniSplitjoin.split_at(positions)
+end
+
+MiniSplitjoin.join = function(pos, opts)
+  pos = pos or vim.api.nvim_win_get_cursor(0)
+  opts = H.get_opts(opts)
+
+  local region = H.find_smallest_bracket_region(pos, opts.brackets)
+  if region == nil then return end
+
+  MiniSplitjoin.join_at(region.from.line, region.to.line)
+end
+
+MiniSplitjoin.split_at = function(positions)
+  positions = H.sort_positions(positions)
+  local n_pos = #positions
+
+  -- Cache intermediately used values
+  local cache = { ci = vim.bo.copyindent, ai = vim.bo.autoindent, si = vim.bo.smartindent, cin = vim.bo.cindent }
+  vim.bo.copyindent, vim.bo.autoindent, vim.bo.smartindent, vim.bo.cindent = true, false, false, false
 
   local cursor_extmark = H.put_extmark_at_cursor()
 
-  -- Perform split consecutively ignoring indent options
-  local cache = { ai = vim.bo.autoindent, si = vim.bo.smartindent, cin = vim.bo.cindent }
-  vim.bo.autoindent, vim.bo.smartindent, vim.bo.cindent = true, false, false
-
+  -- Split
   local shiftwidth = vim.fn.shiftwidth()
   local tab = vim.bo.expandtab and string.rep(' ', shiftwidth) or '\t'
-  local bs = string.rep(H.keys.bs, vim.bo.expandtab and shiftwidth or 1)
-  local n = #split_extmarks
-  for i = 1, n do
-    H.put_cursor_at_extmark(split_extmarks[i])
-    local indent_key = i == 1 and (i == n and '' or tab) or (i < n and '' or bs)
-    vim.cmd('normal! a\r' .. indent_key .. '\27')
+  for i = n_pos, 1, -1 do
+    vim.api.nvim_win_set_cursor(0, positions[i])
+
+    -- Treat last one specially to split only between edge positions
+    local insert_enter = i == n_pos and 'i' or 'a'
+    local indent_keys = i == n_pos and '' or tab
+    vim.cmd('normal! ' .. insert_enter .. '\r' .. indent_keys .. '\27')
   end
 
-  vim.bo.autoindent, vim.bo.smartindent, vim.bo.cindent = cache.ai, cache.si, cache.cin
+  -- Remove trailing whitespace in inner lines
+  local from_line, to_line = positions[1][1] + 1, positions[n_pos][1] + n_pos - 1
+  vim.cmd('keeppatterns ' .. from_line .. ',' .. to_line .. [[s/\s*$//]])
 
+  -- Put cursor back on tracked position
   H.put_cursor_at_extmark(cursor_extmark)
 
-  -- Clear namespace
-  vim.api.nvim_buf_clear_namespace(0, H.ns_id, 0, -1)
+  -- Clear up
+  vim.bo.copyindent, vim.bo.autoindent, vim.bo.smartindent, vim.bo.cindent = cache.ci, cache.ai, cache.si, cache.cin
 end
 
-MiniSplitjoin.join = function(from_line, to_line)
+MiniSplitjoin.join_at = function(from_line, to_line)
+  if to_line <= from_line then return end
+
   -- Join preserving cursor position
   local cursor_extmark = H.put_extmark_at_cursor()
 
-  local join_command = string.format('%s,%sjoin', from_line, to_line)
+  -- - Use `keepmarks` to preserve `[`/`]` marks
+  local join_command = string.format('keepmarks %s,%sjoin', from_line, to_line)
   vim.cmd(join_command)
 
   H.put_cursor_at_extmark(cursor_extmark)
-
-  -- Clear namespace
-  vim.api.nvim_buf_clear_namespace(0, H.ns_id, 0, -1)
 end
 
 -- Helper data ================================================================
@@ -180,10 +192,6 @@ end
 H.default_config = MiniSplitjoin.config
 
 H.ns_id = vim.api.nvim_create_namespace('MiniSplitjoin')
-
-H.keys = {
-  bs = vim.api.nvim_replace_termcodes('<B,S>', true, true, true),
-}
 
 -- Helper functionality =======================================================
 -- Settings -------------------------------------------------------------------
@@ -195,8 +203,9 @@ H.setup_config = function(config)
 
   --stylua: ignore
   vim.validate({
-    ['brackets']   = { config.brackets,   'table', true },
-    ['separators'] = { config.separators, 'table', true },
+    brackets   = { config.brackets,   'table', true },
+    separator = { config.separators, 'string', true },
+    exclude_regions = { config.exclude_regions, 'table', true },
   })
 
   return config
@@ -213,198 +222,39 @@ H.get_config = function(config)
   return vim.tbl_deep_extend('force', MiniSplitjoin.config, vim.b.minisplitjoin_config or {}, config or {})
 end
 
+H.get_opts = function(opts)
+  opts = opts or {}
+
+  -- Can't use usual `vim.tbl_deep_extend()` because it doesn't work properly on arrays
+  local config = H.get_config()
+  local brackets = opts.brackets or config.brackets or { '%b()', '%b[]', '%b{}' }
+  local separator = opts.separator or config.separator or ','
+  local exclude_regions = opts.exclude_regions or config.exclude_regions or { '%b()', '%b[]', '%b{}', '%b""', "%b''" }
+
+  return { brackets = brackets, separator = separator, exclude_regions = exclude_regions }
+end
+
 -- Regions --------------------------------------------------------------------
-H.find_separators_in_region = function(region, brackets, separators)
-  brackets = H.get_config().brackets
-  separators = H.get_config().separators
+H.find_smallest_bracket_region = function(pos, brackets)
+  pos = pos or vim.api.nvim_win_get_cursor(0)
+  brackets = brackets or { '%b()', '%b[]', '%b{}' }
 
-  local from, to = region.from, region.to
-
-  -- Define which separators to skip and when to stop search
-  local res = {}
-  local register = function()
-    -- Ignore separators in strings
-    if H.is_cursor_on_string() then return true end
-
-    -- Stop search (by "not ignoring" item) if gone outside of region
-    local cur_pos = vim.api.nvim_win_get_cursor(0)
-    local line, col = cur_pos[1], cur_pos[2] + 1
-    if to.line < line or (to.line == line and to.col < col) then return false end
-
-    -- Ignore separators inside nested brackets
-    if not vim.deep_equal(region, H.find_smallest_bracket_region(brackets)) then return true end
-
-    -- Register this match but "ignore it" to continute search
-    table.insert(res, { line = line, col = col })
-    return true
-  end
-
-  -- Make search of separators inside region
-  local init_pos = vim.api.nvim_win_get_cursor(0)
-  vim.api.nvim_win_set_cursor(0, { from.line, from.col - 1 })
-
-  local pattern = table.concat(separators, [[\|]])
-  vim.fn.search(pattern, 'nWz', nil, nil, register)
-
-  vim.api.nvim_win_set_cursor(0, init_pos)
-
-  return res
-end
-
-H.find_smallest_bracket_region_prev = function(brackets)
-  brackets = brackets or H.get_config().brackets
-
-  -- Find all regions
-  local cur_pos = vim.api.nvim_win_get_cursor(0)
-  local skip = H.make_skip(cur_pos)
-
-  local regions = {}
-  for _, br in ipairs(brackets) do
-    local from, to = H.find_surrounding_region(br:sub(1, 1), br:sub(2, 2), skip)
-    local is_valid_from, is_valid_to = from.line ~= 0 or from.col ~= 0, to.line ~= 0 or to.col ~= 0
-    if is_valid_from and is_valid_to then table.insert(regions, { from = from, to = to }) end
-  end
-  if #regions == 0 then return nil end
-
-  -- Compute smallest region
-  local line_bytes = H.get_line_offset()
-
-  local res, cur_byte_diff = {}, math.huge
-  for _, r in ipairs(regions) do
-    local byte_from, byte_to = line_bytes[r.from.line] + r.from.col, line_bytes[r.to.line] + r.to.col
-    local byte_diff = byte_to - byte_from
-    if byte_diff < cur_byte_diff then
-      res, cur_byte_diff = r, byte_diff
-    end
-  end
-
-  return res
-end
-
-H.find_surrounding_region = function(left, right, skip)
-  local left_pattern, right_pattern = [[\V]] .. left, [[\V]] .. right
-  local searchpairpos = function(flags)
-    -- local res = vim.fn.searchpairpos(left_pattern, '', right_pattern, 'nWz' .. flags, skip)
-    local res = vim.fn.searchpairpos(left_pattern, '', right_pattern, 'nWz' .. flags, H.is_cursor_on_string)
-    return { line = res[1], col = res[2] }
-  end
-
-  local row, col = vim.fn.line('.'), vim.fn.col('.')
-  local char_at_cursor = vim.fn.getline(row):sub(col, col)
-
-  if char_at_cursor == left then return { line = row, col = col }, searchpairpos('') end
-  if char_at_cursor == right then return searchpairpos('b'), { line = row, col = col } end
-  return searchpairpos('b'), searchpairpos('')
-end
-
-H.get_line_offset = function()
-  -- Compute number of bytes at line starts
-  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
-  local res, cur_byte = {}, 0
-  for i, l in ipairs(lines) do
-    res[i] = cur_byte
-    cur_byte = cur_byte + l:len() + 1
-  end
-  return res
-end
-
-H.make_skip = function(position)
-  -- Try tree-sitter
-  local skip_regions = H.make_treesitter_skip_regions(position[1], position[2])
-  if skip_regions == nil then return [[synIDattr(synID(line("."), col("."), 0), "name") =~? "string\|comment"]] end
-
-  return function()
-    local cur_pos = vim.api.nvim_win_get_cursor(0)
-    return H.is_pos_in_skip(cur_pos[1], cur_pos[2], skip_regions)
-  end
-end
-
-H.make_treesitter_skip_regions = function(line, col)
-  local lang = vim.bo.filetype
-  local ok, parser = pcall(vim.treesitter.get_parser, 0, lang)
-  if not ok then return nil end
-
-  local skip_regions = {}
-  for i = 1, vim.api.nvim_buf_line_count(0) do
-    skip_regions[i] = {}
-  end
-
-  local string_regions = H.find_treesitter_regions(parser, 'string')
-  if not H.is_pos_in_regions(line, col, string_regions) then H.append_to_skip_regions(skip_regions, string_regions) end
-
-  local comment_regions = H.find_treesitter_regions(parser, 'comment')
-  if not H.is_pos_in_regions(line, col, comment_regions) then
-    H.append_to_skip_regions(skip_regions, comment_regions)
-  end
-
-  return skip_regions
-end
-
-H.append_to_skip_regions = function(skip_regions, regions)
-  for _, r in ipairs(regions) do
-    if r[1] == r[3] then
-      local l_num = r[1]
-      table.insert(skip_regions[l_num], { r[2], r[4] })
-    else
-      table.insert(skip_regions[r[1]], { r[2], math.huge })
-      for i = r[1] + 1, r[3] - 1 do
-        skip_regions[i].all = true
-      end
-      table.insert(skip_regions[r[3]], { 0, r[4] })
-    end
-  end
-end
-
-H.is_pos_in_skip = function(line, col, skip_regions)
-  if skip_regions[line].all then return true end
-
-  for _, span in ipairs(skip_regions[line]) do
-    if span[1] <= col and col < span[2] then return true end
-  end
-  return false
-end
-
-H.is_pos_in_regions = function(line, col, regions)
-  for _, r in ipairs(regions) do
-    if r[1] <= line and line <= r[3] and r[2] <= col and col < r[4] then return true end
-  end
-  return false
-end
-
-H.find_treesitter_regions = function(parser, node_name)
-  local query = vim.treesitter.query.parse_query(parser:lang(), '(' .. node_name .. ') @_capture')
-
-  local ranges = {}
-  for _, tree in ipairs(parser:trees()) do
-    for _, node, _ in query:iter_captures(tree:root(), 0) do
-      local row1, col1, row2, col2 = node:range()
-      -- Make region lines 1-based end-inclusive, columns 0-based end-exclusive
-      table.insert(ranges, { row1 + 1, col1, row2 + 1, col2 })
-    end
-  end
-
-  return ranges
-end
-
-H.find_smallest_bracket_region = function(brackets)
-  brackets = brackets or { '()', '[]', '{}' }
-
-  local patterns = vim.tbl_map(function(x) return '%b' .. x end, brackets)
   local neigh = H.get_neighborhood()
-  local cur_offset = neigh.pos_to_offset(H.get_cursor_pos())
+  local cur_offset = neigh.pos_to_offset({ line = pos[1], col = pos[2] + 1 })
 
-  local best_span = H.find_smallest_covering(neigh['1d'], cur_offset + 1, patterns)
+  local best_span = H.find_smallest_covering(neigh['1d'], cur_offset, brackets)
   if best_span == nil then return nil end
+
   return neigh.span_to_region(best_span)
 end
 
-H.find_smallest_covering = function(line, ref_pos, patterns)
+H.find_smallest_covering = function(line, ref_offset, patterns)
   local res, min_width = nil, math.huge
   for _, pattern in ipairs(patterns) do
     local cur_init = 0
     local left, right = string.find(line, pattern, cur_init)
     while left do
-      if left <= ref_pos and ref_pos <= right and (right - left) < min_width then
+      if left <= ref_offset and ref_offset <= right and (right - left) < min_width then
         res, min_width = { from = left, to = right }, right - left
       end
 
@@ -416,9 +266,56 @@ H.find_smallest_covering = function(line, ref_pos, patterns)
   return res
 end
 
+H.find_split_positions = function(region, separator, exclude_regions)
+  local neigh = H.get_neighborhood()
+  local region_span = neigh.region_to_span(region)
+  local region_s = neigh['1d']:sub(region_span.from, region_span.to)
+
+  -- Match separator endings
+  local seps = {}
+  region_s:gsub(separator .. '()', function(r) table.insert(seps, r - 1) end)
+
+  -- Remove separators that are in excluded regions
+  local inner_string, forbidden = region_s:sub(2, -2), {}
+  local add_to_forbidden = function(l, r) table.insert(forbidden, { l + 1, r }) end
+
+  for _, pat in ipairs(exclude_regions) do
+    inner_string:gsub('()' .. pat .. '()', add_to_forbidden)
+  end
+
+  local sub_offsets = vim.tbl_filter(function(x) return not H.is_offset_inside_spans(x, forbidden) end, seps)
+
+  -- Treat enclosing brackets as separators
+  if region_s:len() > 2 then
+    -- Use only last bracket in case of empty brackets
+    table.insert(sub_offsets, 1, 1)
+  end
+  table.insert(sub_offsets, region_s:len())
+
+  -- Convert offsets to positions
+  local start_offset = region_span.from
+  return vim.tbl_map(function(sub_off)
+    local res = neigh.offset_to_pos(start_offset + sub_off - 1)
+    -- Convert to `nvim_win_get_cursor()` format
+    return { res.line, res.col - 1 }
+  end, sub_offsets)
+end
+
+H.is_offset_inside_spans = function(ref_point, spans)
+  for _, span in ipairs(spans) do
+    if span[1] <= ref_point and ref_point <= span[2] then return true end
+  end
+  return false
+end
+
 -- Simplified version of "neighborhood" from 'mini.ai':
 -- - Use whol buffer.
 -- - No empty regions or spans.
+--
+-- NOTEs:
+-- - `region = { from = { line = a, col = b }, to = { line = c, col = d } }`.
+--   End-inclusive charwise selection. All `a`, `b`, `c`, `d` are 1-based.
+-- - `offset` is the number between 1 to `neigh1d:len()`.
 H.get_neighborhood = function()
   local neigh2d = vim.api.nvim_buf_get_lines(0, 0, -1, false)
   -- Append 'newline' character to distinguish between lines in 1d case
@@ -427,8 +324,15 @@ H.get_neighborhood = function()
     neigh2d[k] = v .. '\n'
   end
   local neigh1d = table.concat(neigh2d, '')
-  local line_offsets = H.get_line_offset()
   local n_lines = #neigh2d
+
+  -- Compute offsets for just before line starts
+  local line_offsets = {}
+  local cur_offset = 0
+  for i = 1, n_lines do
+    line_offsets[i] = cur_offset
+    cur_offset = cur_offset + neigh2d[i]:len()
+  end
 
   -- Convert 2d buffer position to 1d offset
   local pos_to_offset = function(pos) return line_offsets[pos.line] + pos.col end
@@ -436,7 +340,7 @@ H.get_neighborhood = function()
   -- Convert 1d offset to 2d buffer position
   local offset_to_pos = function(offset)
     for i = 1, n_lines - 1 do
-      if line_offsets[i] <= offset and offset < line_offsets[i + 1] then
+      if line_offsets[i] < offset and offset <= line_offsets[i + 1] then
         return { line = i, col = offset - line_offsets[i] }
       end
     end
@@ -470,27 +374,16 @@ end
 H.put_cursor_at_extmark = function(id)
   local new_pos = vim.api.nvim_buf_get_extmark_by_id(0, H.ns_id, id, {})
   vim.api.nvim_win_set_cursor(0, { new_pos[1] + 1, new_pos[2] })
-end
-
-H.is_cursor_on_string = function()
-  -- If tree-sitter is active, use it. Otherwise use built-in syntax.
-  local ok, captures = pcall(vim.treesitter.get_captures_at_cursor, 0)
-  if ok then
-    -- NOTE: this **really** slows down `searchpairpos` in big file with many
-    -- intermediate matches
-    return vim.tbl_contains(captures, 'string')
-  else
-    local attr_name = vim.fn.synIDattr(vim.fn.synID(vim.fn.line('.'), vim.fn.col('.'), 0), 'name')
-    return attr_name:lower():find('string') ~= nil
-  end
+  vim.api.nvim_buf_del_extmark(0, H.ns_id, id)
 end
 
 -- Utilities ------------------------------------------------------------------
 H.error = function(msg) error(string.format('(mini.splitjoin) %s', msg), 0) end
 
-H.get_cursor_pos = function()
-  local pos = vim.api.nvim_win_get_cursor(0)
-  return { line = pos[1], col = pos[2] }
+H.sort_positions = function(positions)
+  local res = vim.deepcopy(positions)
+  table.sort(res, function(a, b) return a[1] < b[1] or (a[1] == b[1] and a[2] < b[2]) end)
+  return res
 end
 
 return MiniSplitjoin
