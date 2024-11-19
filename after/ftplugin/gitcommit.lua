9de18aad28c18fef61282383b5bfeabdc41a6b19
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2024-11-19 16:28:01 +0200

    Tweak filetype plugins.

--- after/ftplugin/gitcommit.lua
+++ after/ftplugin/gitcommit.lua
@@ -1,2 +1,3 @@
 -- Using `vim.cmd` instead of `vim.wo` because it is yet more reliable
 vim.cmd('setlocal spell wrap')
+vim.cmd('setlocal foldmethod=expr foldexpr=v:lua.MiniGit.diff_foldexpr() foldlevel=1')
--- after/ftplugin/help.lua
+++ after/ftplugin/help.lua
@@ -1,4 +1,1 @@
 vim.cmd('setlocal nofoldenable')
-
--- Enable 'mini.clue' triggers in help buffer
-if _G.MiniClue ~= nil then MiniClue.enable_buf_triggers(vim.api.nvim_get_current_buf()) end
