86248eef9971517d0ed03042cc1d183c2de601cc
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2024-12-19 11:45:50 +0200

    Tweak curated color schemes.

--- colors/miniautumn.lua
+++ colors/miniautumn.lua
@@ -1,9 +1,9 @@
--- "Cooling red autumn" vibe
+-- "Cooling autumn"
 -- Dark  (OKLch): bg=15-2-315 and fg=85-2-336
--- Light (OKLch): bg=90-2-315 and fg=20-2-336
+-- Light (OKLch): bg=90-1-315 and fg=20-2-336
 -- Foreground hues are picked to maximize palette's bg colors visibility
 local is_dark = vim.o.background == 'dark'
-local bg = is_dark and '#262029' or '#e8dfec'
+local bg = is_dark and '#262029' or '#e5e1e7'
 local fg = is_dark and '#ded0da' or '#352a32'
 
 require('mini.hues').setup({
--- colors/miniwinter.lua
+++ colors/miniwinter.lua
@@ -1,9 +1,9 @@
--- "Gloomy blue winter" vibe
+-- "Icy winter"
 -- Dark  (OKLch): bg=15-3-225 and fg=85-2-270
--- Light (OKLch): bg=90-2-225 and fg=20-2-270
--- Foreground hue is hand picked for good bg colors
+-- Light (OKLch): bg=90-1-225 and fg=20-2-270
+-- Foreground hues are picked to maximize palette's bg colors visibility
 local is_dark = vim.o.background == 'dark'
-local bg = is_dark and '#11262d' or '#d5e6ed'
+local bg = is_dark and '#11262d' or '#dce4e8'
 local fg = is_dark and '#cfd4e2' or '#2a2e39'
 
 require('mini.hues').setup({
